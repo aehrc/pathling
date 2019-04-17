c0ca2f4e59de785536dc672cff06420f7718fbc4
John Grimes <John.Grimes@csiro.au>
2019-04-17 16:21:42 +1000

    Improve performance and usability of element tree

--- ui/src/components/ElementTree/ElementTree.js
+++ ui/src/components/ElementTree/ElementTree.js
@@ -2,12 +2,12 @@
  * Copyright © Australian e-Health Research Centre, CSIRO. All rights reserved.
  */
 
-import React, { useState } from 'react'
+import React, { useState, useReducer } from 'react'
 import { connect } from 'react-redux'
-import { Tree, ContextMenu, Menu, MenuItem } from '@blueprintjs/core'
-import { fromJS, Map, List } from 'immutable'
+import { Tree, ContextMenu, Menu, MenuItem, Icon } from '@blueprintjs/core'
 
-import * as actions from '../../store/Actions'
+import store from '../../store'
+import { addAggregation, addGrouping } from '../../store/Actions'
 import './ElementTree.less'
 import resourceTree from '../../../config/resource-tree.json'
 import complexTypesTree from '../../../config/complex-type-tree.json'
@@ -37,6 +37,7 @@ const supportedComplexTypes = [
   'Timing',
   'Reference',
 ]
+const convertedResourceTree = convertElementTree(resourceTree)
 
 /**
  * Renders a tree showing resources and elements available for use within
@@ -44,169 +45,143 @@ const supportedComplexTypes = [
  *
  * @author John Grimes
  */
-function ElementTree(props) {
-  const { addAggregation, addGrouping } = props
-  const treeForConversion = fromJS(resourceTree)
-  const [tree, setTree] = useState(convertElementTree(treeForConversion))
+function ElementTree() {
+  const [tree, setTree] = useState(convertedResourceTree)
+  const [update, forceUpdate] = useReducer(x => x + 1, 0)
 
-  function handleNodeExpand(_, nodePath) {
-    return updateIsExpanded(nodePath, true)
+  function handleNodeExpand(node) {
+    node.isExpanded = true
+    forceUpdate()
   }
 
-  function handleNodeCollapse(_, nodePath) {
-    return updateIsExpanded(nodePath, false)
+  function handleNodeCollapse(node) {
+    node.isExpanded = false
+    forceUpdate()
   }
 
-  function handleNodeClick(node, nodePath) {
-    const elementType = node.nodeData.elementType,
-      expandable =
-        elementType === 'Resource' ||
-        elementType === 'BackboneElement' ||
-        supportedComplexTypes.includes(elementType)
-    if (expandable && !node.isExpanded) handleNodeExpand(node, nodePath)
-    if (expandable && node.isExpanded) handleNodeCollapse(node, nodePath)
-  }
-
-  function handleNodeContextMenu(node, path, event) {
-    const aggregationExpression = `${node.nodeData.fhirPath}.count()`,
-      aggregationLabel = aggregationExpression,
-      groupingExpression = node.nodeData.fhirPath,
-      groupingLabel = groupingExpression,
-      aggregationMenuItem = (
-        <MenuItem
-          icon="trending-up"
-          text={`Add "${aggregationExpression}" to aggregations`}
-          onClick={() =>
-            addAggregation({
-              expression: aggregationExpression,
-              label: aggregationLabel,
-            })
-          }
-        />
-      ),
-      groupingMenuItem = (
-        <MenuItem
-          icon="graph"
-          text={`Add "${groupingExpression}" to groupings`}
-          onClick={() =>
-            addGrouping({
-              expression: groupingExpression,
-              label: groupingLabel,
-            })
-          }
-        />
-      )
-    event.preventDefault()
-    if (node.nodeData.elementType === 'Element') {
-      ContextMenu.show(
-        <Menu>
-          {aggregationMenuItem}
-          {groupingMenuItem}
-        </Menu>,
-        { left: event.clientX, top: event.clientY },
-      )
-    } else {
-      ContextMenu.show(<Menu>{aggregationMenuItem}</Menu>, {
-        left: event.clientX,
-        top: event.clientY,
-      })
-    }
-  }
-
-  function updateIsExpanded(nodePath, value) {
-    const updatePath = nodePath
-      .reduce(
-        (acc, x, n) =>
-          n !== 0 ? acc.concat(['childNodes', x]) : acc.concat(x),
-        [],
-      )
-      .concat('isExpanded')
-    const updatedTree = List(tree).updateIn(updatePath, () => value)
-    setTree(updatedTree)
-  }
-
-  function convertElementTree(tree) {
-    return tree.keySeq().map((resourceName, key) =>
-      Map({
-        id: resourceName,
-        label: resourceName,
-        childNodes: convertElements(
-          tree.get(resourceName),
-          1,
-          [key],
-          resourceName,
-        ),
-        depth: 1,
-        path: [key],
-        icon: 'cube',
-        nodeData: Map({ fhirPath: resourceName, elementType: 'Resource' }),
-      }),
-    )
-  }
-
-  function convertElements(elements, depth, path, fhirPath) {
-    if (!elements || depth > maxDepth) return null
-    return elements.map((element, key) => {
-      const newDepth = depth + 1
-      const newPath = path.concat(key)
-      const pathTokens = element.get('path').split('.')
-      const newFhirPath = fhirPath + '.' + pathTokens[pathTokens.length - 1]
-      let converted = Map({
-        id: element.get('name'),
-        label: element.get('name'),
-        depth: newDepth,
-        path: newPath,
-        icon: 'symbol-square',
-        nodeData: Map({ fhirPath: newFhirPath }),
-      })
-      if (element.get('children')) {
-        converted = converted.set(
-          'childNodes',
-          convertElements(
-            element.get('children'),
-            newDepth,
-            newPath,
-            newFhirPath,
-          ),
-        )
-        converted = converted.set('icon', 'folder-close')
-        converted = converted.setIn(
-          ['nodeData', 'elementType'],
-          'BackboneElement',
-        )
-      } else if (supportedComplexTypes.includes(element.get('type'))) {
-        converted = converted
-          .set(
-            'childNodes',
-            convertElements(
-              fromJS(complexTypesTree[element.get('type')]),
-              newDepth,
-              newPath,
-              newFhirPath,
-            ),
-          )
-          .setIn(['nodeData', 'elementType'], element.get('type'))
-          .set('icon', 'grid-view')
-      } else {
-        converted = converted.setIn(['nodeData', 'elementType'], 'Element')
-      }
-      return converted
-    })
-  }
-
+  console.log('render')
   return (
     <Tree
       className="element-tree"
-      contents={tree.toJS()}
-      onNodeClick={handleNodeClick}
+      contents={tree}
       onNodeExpand={handleNodeExpand}
       onNodeCollapse={handleNodeCollapse}
-      onNodeContextMenu={handleNodeContextMenu}
     />
   )
 }
 
-export default connect(
-  null,
-  actions,
-)(ElementTree)
+function openContextMenu(event, nodeData) {
+  const aggregationExpression = `${nodeData.fhirPath}.count()`,
+    aggregationLabel = aggregationExpression,
+    groupingExpression = nodeData.fhirPath,
+    groupingLabel = groupingExpression,
+    aggregationMenuItem = (
+      <MenuItem
+        icon="trending-up"
+        text={`Add "${aggregationExpression}" to aggregations`}
+        onClick={() =>
+          store.dispatch(
+            addAggregation({
+              expression: aggregationExpression,
+              label: aggregationLabel,
+            }),
+          )
+        }
+      />
+    ),
+    groupingMenuItem = (
+      <MenuItem
+        icon="graph"
+        text={`Add "${groupingExpression}" to groupings`}
+        onClick={() =>
+          store.dispatch(
+            addGrouping({
+              expression: groupingExpression,
+              label: groupingLabel,
+            }),
+          )
+        }
+      />
+    )
+  if (nodeData.elementType === 'Element') {
+    ContextMenu.show(
+      <Menu>
+        {aggregationMenuItem}
+        {groupingMenuItem}
+      </Menu>,
+      { left: event.clientX, top: event.clientY },
+    )
+  } else {
+    ContextMenu.show(<Menu>{aggregationMenuItem}</Menu>, {
+      left: event.clientX,
+      top: event.clientY,
+    })
+  }
+}
+
+function convertElementTree(tree) {
+  console.log('convertElementTree')
+  return Object.keys(tree).map((resourceName, key) => {
+    const nodeData = { fhirPath: resourceName, elementType: 'Resource' }
+    return {
+      id: resourceName,
+      label: resourceName,
+      childNodes: convertElements(tree[resourceName], 1, [key], resourceName),
+      depth: 1,
+      path: [key],
+      icon: 'cube',
+      nodeData,
+      secondaryLabel: (
+        <Icon icon="add" onClick={event => openContextMenu(event, nodeData)} />
+      ),
+    }
+  })
+}
+
+function convertElements(elements, depth, path, fhirPath) {
+  if (!elements || depth > maxDepth) return null
+  return elements.map((element, key) => {
+    const newDepth = depth + 1
+    const newPath = path.concat(key)
+    const pathTokens = element['path'].split('.')
+    const newFhirPath = fhirPath + '.' + pathTokens[pathTokens.length - 1]
+    let converted = {
+      id: element['name'],
+      label: element['name'],
+      depth: newDepth,
+      path: newPath,
+      icon: 'symbol-square',
+      nodeData: { fhirPath: newFhirPath },
+    }
+    if (element['children']) {
+      converted.childNodes = convertElements(
+        element['children'],
+        newDepth,
+        newPath,
+        newFhirPath,
+      )
+      converted.icon = 'folder-close'
+      converted.nodeData.elementType = 'BackboneElement'
+    } else if (supportedComplexTypes.includes(element['type'])) {
+      converted.childNodes = convertElements(
+        complexTypesTree[element['type']],
+        newDepth,
+        newPath,
+        newFhirPath,
+      )
+      converted.nodeData.elementType = element['type']
+      converted.icon = 'grid-view'
+    } else {
+      converted.nodeData.elementType = 'Element'
+      converted.secondaryLabel = (
+        <Icon
+          icon="add"
+          onClick={event => openContextMenu(event, converted.nodeData)}
+        />
+      )
+    }
+    return converted
+  })
+}
+
+export default ElementTree
