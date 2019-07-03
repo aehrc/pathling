c088cfd41dc3237411b6e05670b991db58bcd763
John Grimes <John.Grimes@csiro.au>
2019-07-03 14:02:47 +1000

    Complete initial closure test

--- fhir-server/src/test/java/au/csiro/clinsight/ClosureTest.java
+++ fhir-server/src/test/java/au/csiro/clinsight/ClosureTest.java
@@ -22,6 +22,7 @@ import org.apache.spark.sql.catalog.Catalog;
 import org.eclipse.jetty.server.Server;
 import org.json.JSONException;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -57,13 +58,17 @@ public class ClosureTest {
 
   @SuppressWarnings("unchecked")
   @Test
+  @Ignore
   public void simpleQuery() throws IOException, JSONException {
     String inParams = "{\n"
-        + "  \"resourceType\": \"Parameters\",\n"
         + "  \"parameter\": [\n"
         + "    {\n"
         + "      \"name\": \"aggregation\",\n"
         + "      \"part\": [\n"
+        + "        {\n"
+        + "          \"name\": \"expression\",\n"
+        + "          \"valueString\": \"Patient.count()\"\n"
+        + "        },\n"
         + "        {\n"
         + "          \"name\": \"label\",\n"
         + "          \"valueString\": \"Number of patients\"\n"
@@ -72,21 +77,96 @@ public class ClosureTest {
         + "    },\n"
         + "    {\n"
         + "      \"name\": \"grouping\",\n"
-        + "      \"part\":[\n"
-        + "        {\n"
-        + "          \"name\": \"label\",\n"
-        + "          \"valueString\": \"Equivalence between encounter diagnosis and procedure reason\"\n"
-        + "        },\n"
+        + "      \"part\": [\n"
         + "        {\n"
         + "          \"name\": \"expression\",\n"
-        + "          \"valueString\": \"Patient.reverseResolve(Encounter.subject).diagnosis.condition.resolve(Condition).code.coding.closure(Patient.reverseResolve(Encounter.subject).reverseResolve(Procedure.encounter).reasonCode.coding)\"\n"
+        + "          \"valueString\": \"'subsumes' in Patient.reverseResolve(Encounter.subject).where(https://csiro.au/fhir/CodeSystem/kidgen-pilot-encounter-type-0|pre-investigation in $this.type.coding).reverseResolve(Condition.context).code.coding.closure(Patient.reverseResolve(Encounter.subject).where(https://csiro.au/fhir/CodeSystem/kidgen-pilot-encounter-type-0|referral in $this.type.coding).reverseResolve(Condition.context).code.coding)\"\n"
+        + "        },\n"
+        + "        {\n"
+        + "          \"name\": \"label\",\n"
+        + "          \"valueString\": \"Pre-investigation diagnosis more specific than referral diagnosis?\"\n"
         + "        }\n"
         + "      ]\n"
         + "    }\n"
-        + "  ]\n"
-        + "}\n";
+        + "  ],\n"
+        + "  \"resourceType\": \"Parameters\"\n"
+        + "}";
 
-    String expectedSql = "";
+    String expectedSql1 = "SELECT encounterConditionAsContextCodeCoding.system, "
+        + "encounterConditionAsContextCodeCoding.version, "
+        + "encounterConditionAsContextCodeCoding.code, "
+        + "encounterConditionAsContextCodeCoding.display, "
+        + "encounterConditionAsContextCodeCoding.userSelected "
+        + "FROM patient "
+        + "LEFT JOIN encounter patientEncounterAsSubject "
+        + "ON patient.id = patientEncounterAsSubject.subject.reference "
+        + "LEFT JOIN condition encounterConditionAsContext "
+        + "ON patientEncounterAsSubject.id = encounterConditionAsContext.context.reference "
+        + "LATERAL VIEW OUTER explode(encounterConditionAsContext.code.coding) encounterConditionAsContextCodeCoding AS encounterConditionAsContextCodeCoding "
+        + "UNION "
+        + "SELECT encounterProcedureRequestAsContextReasonCodeCoding.system, "
+        + "encounterProcedureRequestAsContextReasonCodeCoding.version, "
+        + "encounterProcedureRequestAsContextReasonCodeCoding.code, "
+        + "encounterProcedureRequestAsContextReasonCodeCoding.display, "
+        + "encounterProcedureRequestAsContextReasonCodeCoding.userSelected "
+        + "FROM patient "
+        + "LEFT JOIN encounter patientEncounterAsSubject "
+        + "ON patient.id = patientEncounterAsSubject.subject.reference "
+        + "LEFT JOIN procedurerequest encounterProcedureRequestAsContext "
+        + "ON patientEncounterAsSubject.id = encounterProcedureRequestAsContext.context.reference "
+        + "LATERAL VIEW OUTER explode(encounterProcedureRequestAsContext.reasonCode) encounterProcedureRequestAsContextReasonCode AS encounterProcedureRequestAsContextReasonCode "
+        + "LATERAL VIEW OUTER explode(encounterProcedureRequestAsContextReasonCode.coding) encounterProcedureRequestAsContextReasonCodeCoding AS encounterProcedureRequestAsContextReasonCodeCoding";
+
+    String expectedSql2 =
+        "SELECT patientEncounterAsSubjectConditionAsContextReferralPreInvestigationClosureInResult.inResult AS `Pre-investigation diagnosis more specific than referral diagnosis?`, "
+            + "COUNT(DISTINCT patient.id) AS `Number of patients` "
+            + "FROM patient "
+            + "LEFT JOIN ("
+            + "SELECT patient.id, "
+            + "MAX(CASE WHEN patientEncounterAsSubjectConditionAsContextReferralPreInvestigationClosure.equivalence = 'subsumes' THEN TRUE ELSE FALSE END) AS inResult "
+            + "FROM patient "
+            + "LEFT JOIN ("
+            + "SELECT * "
+            + "FROM encounter "
+            + "LATERAL VIEW OUTER explode(encounter.type) encounterType AS encounterType "
+            + "LATERAL VIEW OUTER explode(encounterType.coding) encounterTypeCoding AS encounterTypeCoding "
+            + "WHERE encounterTypeCoding.code = 'referral'"
+            + ") patientEncounterAsSubjectReferral "
+            + "ON patient.id = patientEncounterAsSubjectReferral.subject.reference "
+            + "LEFT JOIN condition patientEncounterAsSubjectReferralConditionAsContext "
+            + "ON patientEncounterAsSubjectReferral.id = patientEncounterAsSubjectReferralConditionAsContext.context.reference "
+            + "LEFT JOIN ("
+            + "SELECT * "
+            + "FROM condition "
+            + "LATERAL VIEW OUTER explode(condition.code.coding) conditionCodeCoding AS conditionCodeCoding"
+            + ") patientEncounterAsSubjectReferralConditionAsContextCodeCodingExploded "
+            + "ON patientEncounterAsSubjectReferralConditionAsContext.id = patientEncounterAsSubjectReferralConditionAsContextCodeCodingExploded.id "
+            + "LEFT JOIN ("
+            + "SELECT * "
+            + "FROM encounter "
+            + "LATERAL VIEW OUTER explode(encounter.type) encounterType AS encounterType "
+            + "LATERAL VIEW OUTER explode(encounterType.coding) encounterTypeCoding AS encounterTypeCoding "
+            + "WHERE encounterTypeCoding.code = 'pre-investigation'"
+            + ") patientEncounterAsSubjectPreInvestigation "
+            + "ON patient.id = patientEncounterAsSubjectPreInvestigation.subject.reference "
+            + "LEFT JOIN condition patientEncounterAsSubjectPreInvestigationConditionAsContext "
+            + "ON patientEncounterAsSubjectPreInvestigation.id = patientEncounterAsSubjectPreInvestigationConditionAsContext.context.reference "
+            + "LEFT JOIN ("
+            + "SELECT * "
+            + "FROM condition "
+            + "LATERAL VIEW OUTER explode(condition.code.coding) conditionCodeCoding AS conditionCodeCoding"
+            + ") patientEncounterAsSubjectPreInvestigationConditionAsContextCodeCodingExploded "
+            + "ON patientEncounterAsSubjectPreInvestigationConditionAsContext.id = patientEncounterAsSubjectPreInvestigationConditionAsContextCodeCodingExploded.id "
+            + "LEFT JOIN `closure_simpleQuery` patientEncounterAsSubjectConditionAsContextReferralPreInvestigationClosure "
+            + "ON patientEncounterAsSubjectReferralConditionAsContextCodeCodingExploded.conditionCodeCoding.system = patientEncounterAsSubjectConditionAsContextReferralPreInvestigationClosure.targetSystem "
+            + "AND patientEncounterAsSubjectReferralConditionAsContextCodeCodingExploded.conditionCodeCoding.code = patientEncounterAsSubjectConditionAsContextReferralPreInvestigationClosure.targetCode "
+            + "AND patientEncounterAsSubjectPreInvestigationConditionAsContextCodeCodingExploded.conditionCodeCoding.system = patientEncounterAsSubjectConditionAsContextReferralPreInvestigationClosure.sourceSystem "
+            + "AND patientEncounterAsSubjectPreInvestigationConditionAsContextCodeCodingExploded.conditionCodeCoding.code = patientEncounterAsSubjectConditionAsContextReferralPreInvestigationClosure.sourceCode "
+            + "GROUP BY 1"
+            + ") patientEncounterAsSubjectConditionAsContextReferralPreInvestigationClosureInResult "
+            + "ON patient.id = patientEncounterAsSubjectConditionAsContextReferralPreInvestigationClosureInResult.id "
+            + "GROUP BY 1 "
+            + "ORDER BY 1, 2";
 
     Dataset mockDataset = createMockDataset();
     when(mockSpark.sql(any())).thenReturn(mockDataset);
@@ -96,6 +176,7 @@ public class ClosureTest {
     httpClient.execute(httpPost);
 
     verify(mockSpark).sql("USE clinsight");
-    verify(mockSpark).sql(expectedSql);
+    verify(mockSpark).sql(expectedSql1);
+    verify(mockSpark).sql(expectedSql2);
   }
 }
