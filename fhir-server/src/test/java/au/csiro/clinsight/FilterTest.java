5bcc34dbfd5406984ee3b8876d5a812e732fb34c
John Grimes <John.Grimes@csiro.au>
2019-07-09 10:51:33 +1000

    Add tests for membership expressions and where function

--- fhir-server/src/test/java/au/csiro/clinsight/FilterTest.java
+++ fhir-server/src/test/java/au/csiro/clinsight/FilterTest.java
@@ -27,6 +27,7 @@ import org.eclipse.jetty.server.Server;
 import org.json.JSONException;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 
@@ -373,6 +374,88 @@ public class FilterTest {
         "SELECT COUNT(DISTINCT patient.id) AS `Number of patients` "
             + "FROM patient "
             + "WHERE patient.deceasedBoolean = TRUE";
+
+    Dataset mockDataset = createMockDataset();
+    when(mockSpark.sql(any())).thenReturn(mockDataset);
+    when(mockDataset.collectAsList()).thenReturn(new ArrayList());
+
+    HttpPost httpPost = postFhirResource(inParams, QUERY_URL);
+    httpClient.execute(httpPost);
+
+    verify(mockSpark).sql("USE clinsight");
+    verify(mockSpark).sql(expectedSql);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  @Ignore
+  public void codingLiteral() throws IOException {
+    String inParams = "{\n"
+        + "  \"parameter\": [\n"
+        + "    {\n"
+        + "      \"name\": \"aggregation\",\n"
+        + "      \"part\": [\n"
+        + "        {\n"
+        + "          \"name\": \"expression\",\n"
+        + "          \"valueString\": \"Patient.count()\"\n"
+        + "        },\n"
+        + "        {\n"
+        + "          \"name\": \"label\",\n"
+        + "          \"valueString\": \"Number of patients\"\n"
+        + "        }\n"
+        + "      ]\n"
+        + "    },\n"
+        + "    {\n"
+        + "      \"name\": \"filter\",\n"
+        + "      \"valueString\": \"http://snomed.info/sct|40275004 in Patient.reverseResolve(Condition.subject).code.coding\"\n"
+        + "    }\n"
+        + "  ],\n"
+        + "  \"resourceType\": \"Parameters\"\n"
+        + "}";
+
+    String expectedSql =
+        "SELECT a.result AS `Diagnosed with contact dermatitis`, COUNT(DISTINCT patient.id) AS `Number of patients` FROM patient LEFT JOIN (SELECT patient.id, IFNULL(MAX(patientConditionAsSubjectCodeCoding.system = 'https://snomed.info/sct' AND patientConditionAsSubjectCodeCoding.code = '40275004'), FALSE) OR IFNULL(MAX(patientConditionAsSubjectCodeCodingClosure.equivalence = 'subsumes' OR patientConditionAsSubjectCodeCodingClosure.equivalence = 'equals'), FALSE) AS result FROM patient LEFT JOIN condition patientConditionAsSubject ON patient.id = patientConditionAsSubject.subject.reference LATERAL VIEW OUTER explode(patientConditionAsSubject.code.coding) patientConditionAsSubjectCodeCoding AS patientConditionAsSubjectCodeCoding LEFT JOIN closure patientConditionAsSubjectCodeCodingClosure ON patientConditionAsSubjectCodeCoding.system = patientConditionAsSubjectCodeCodingClosure.sourceSystem AND patientConditionAsSubjectCodeCoding.code = patientConditionAsSubjectCodeCodingClosure.sourceCode GROUP BY 1) a ON patient.id = a.id GROUP BY 1 ORDER BY 1, 2";
+
+    Dataset mockDataset = createMockDataset();
+    when(mockSpark.sql(any())).thenReturn(mockDataset);
+    when(mockDataset.collectAsList()).thenReturn(new ArrayList());
+
+    HttpPost httpPost = postFhirResource(inParams, QUERY_URL);
+    httpClient.execute(httpPost);
+
+    verify(mockSpark).sql("USE clinsight");
+    verify(mockSpark).sql(expectedSql);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  @Ignore
+  public void membershipExpression() throws IOException {
+    String inParams = "{\n"
+        + "  \"parameter\": [\n"
+        + "    {\n"
+        + "      \"name\": \"aggregation\",\n"
+        + "      \"part\": [\n"
+        + "        {\n"
+        + "          \"name\": \"expression\",\n"
+        + "          \"valueString\": \"Patient.count()\"\n"
+        + "        },\n"
+        + "        {\n"
+        + "          \"name\": \"label\",\n"
+        + "          \"valueString\": \"Number of patients\"\n"
+        + "        }\n"
+        + "      ]\n"
+        + "    },\n"
+        + "    {\n"
+        + "      \"name\": \"filter\",\n"
+        + "      \"valueString\": \"'40275004' in Patient.reverseResolve(Condition.subject).code.coding.code\"\n"
+        + "    }\n"
+        + "  ],\n"
+        + "  \"resourceType\": \"Parameters\"\n"
+        + "}";
+
+    String expectedSql =
+        "SELECT a.result AS `Diagnosed with contact dermatitis`, COUNT(DISTINCT patient.id) AS `Number of patients` FROM patient LEFT JOIN (SELECT patient.id, IFNULL(MAX(patientConditionAsSubjectCodeCoding.system = 'http://snomed.info/sct' AND patientConditionAsSubjectCodeCoding.code = '40275004'), FALSE) AS result FROM patient LEFT JOIN condition patientConditionAsSubject ON patient.id = patientConditionAsSubject.subject.reference LATERAL VIEW OUTER explode(patientConditionAsSubject.code.coding) patientConditionAsSubjectCodeCoding AS patientConditionAsSubjectCodeCoding GROUP BY 1) a ON patient.id = a.id GROUP BY 1 ORDER BY 1, 2";
 
     Dataset mockDataset = createMockDataset();
     when(mockSpark.sql(any())).thenReturn(mockDataset);
---
+++ fhir-server/src/test/java/au/csiro/clinsight/WhereFunctionTest.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright © Australian e-Health Research Centre, CSIRO. All rights reserved.
+ */
+
+package au.csiro.clinsight;
+
+import static au.csiro.clinsight.TestConfiguration.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import au.csiro.clinsight.fhir.AnalyticsServerConfiguration;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.SparkSession;
+import org.eclipse.jetty.server.Server;
+import org.json.JSONException;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author John Grimes
+ */
+public class WhereFunctionTest {
+
+  private static final String QUERY_URL = FHIR_SERVER_URL + "/$aggregate-query";
+  private Server server;
+  private SparkSession mockSpark;
+  private CloseableHttpClient httpClient;
+
+  @Before
+  public void setUp() throws Exception {
+    TerminologyClient mockTerminologyClient = mock(TerminologyClient.class);
+    mockSpark = mock(SparkSession.class);
+    mockDefinitionRetrieval(mockTerminologyClient);
+
+    AnalyticsServerConfiguration configuration = new AnalyticsServerConfiguration();
+    configuration.setTerminologyClient(mockTerminologyClient);
+    configuration.setSparkSession(mockSpark);
+    configuration.setExplainQueries(false);
+
+    server = startFhirServer(configuration);
+    httpClient = HttpClients.createDefault();
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  @Ignore
+  public void simpleQuery() throws IOException, JSONException {
+    String inParams = "{\n"
+        + "  \"parameter\": [\n"
+        + "    {\n"
+        + "      \"name\": \"aggregation\",\n"
+        + "      \"part\": [\n"
+        + "        {\n"
+        + "          \"name\": \"expression\",\n"
+        + "          \"valueString\": \"Patient.count()\"\n"
+        + "        },\n"
+        + "        {\n"
+        + "          \"name\": \"label\",\n"
+        + "          \"valueString\": \"Number of patients\"\n"
+        + "        }\n"
+        + "      ]\n"
+        + "    },\n"
+        + "    {\n"
+        + "      \"name\": \"grouping\",\n"
+        + "      \"part\": [\n"
+        + "        {\n"
+        + "          \"name\": \"expression\",\n"
+        + "          \"valueString\": \"Patient.reverseResolve(Encounter.subject).where($this.class.code = 'ambulatory').type.coding.display\"\n"
+        + "        },\n"
+        + "        {\n"
+        + "          \"name\": \"label\",\n"
+        + "          \"valueString\": \"Encounter type, where ambulatory\"\n"
+        + "        }\n"
+        + "      ]\n"
+        + "    }\n"
+        + "  ],\n"
+        + "  \"resourceType\": \"Parameters\"\n"
+        + "}";
+
+    String expectedSql = "SELECT patientEncounterAsSubjectTypeCoding.display AS `Encounter type, where ambulatory`, COUNT(DISTINCT patient.id) AS `Number of patients` FROM patient LEFT JOIN encounter patientEncounterAsSubject ON patient.id = patientEncounterAsSubject.subject.reference AND patientEncounterAsSubject.class.code = 'ambulatory' LATERAL VIEW OUTER explode(patientEncounterAsSubject.type) patientEncounterAsSubjectType AS patientEncounterAsSubjectType LATERAL VIEW OUTER explode(patientEncounterAsSubjectType.coding) patientEncounterAsSubjectTypeCoding AS patientEncounterAsSubjectTypeCoding GROUP BY 1 ORDER BY 1, 2";
+
+    Dataset mockDataset = createMockDataset();
+    when(mockSpark.sql(any())).thenReturn(mockDataset);
+    when(mockDataset.collectAsList()).thenReturn(new ArrayList());
+
+    HttpPost httpPost = postFhirResource(inParams, QUERY_URL);
+    httpClient.execute(httpPost);
+
+    verify(mockSpark).sql("USE clinsight");
+    verify(mockSpark).sql(expectedSql);
+  }
+
+}
