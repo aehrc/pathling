f5f1dac7c2b9d99a2e88726ad619124b21d31335
John Grimes <John.Grimes@csiro.au>
2020-10-25 12:11:26 +1000

    Order data by ID ascending before write
    This seems to reduce the amount of sorting necessary during runtime
    query.

--- fhir-server/src/main/java/au/csiro/pathling/io/ResourceWriter.java
+++ fhir-server/src/main/java/au/csiro/pathling/io/ResourceWriter.java
@@ -8,6 +8,7 @@ package au.csiro.pathling.io;
 
 import static au.csiro.pathling.io.PersistenceScheme.convertS3ToS3aUrl;
 import static au.csiro.pathling.io.PersistenceScheme.fileNameForResource;
+import static org.apache.spark.sql.functions.asc;
 
 import au.csiro.pathling.Configuration;
 import javax.annotation.Nonnull;
@@ -48,7 +49,10 @@ public class ResourceWriter {
   public void write(@Nonnull final ResourceType resourceType, @Nonnull final Dataset resources) {
     final String tableUrl =
         warehouseUrl + "/" + databaseName + "/" + fileNameForResource(resourceType);
-    resources.write().mode(SaveMode.Overwrite).parquet(tableUrl);
+    resources.orderBy(asc("id"))
+        .write()
+        .mode(SaveMode.Overwrite)
+        .parquet(tableUrl);
   }
 
 }
