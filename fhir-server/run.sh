abb1805fdb804ea6590c00177a43e2c64fc60bc8
John Grimes <John.Grimes@csiro.au>
2021-01-04 15:31:13 +1000

    Fix Hadoop Native problem

--- fhir-server/run.sh
+++ fhir-server/run.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-java $JAVA_OPTS -ea -jar /pathling.jar -Djava.library.path=/opt/hadoop/lib/native
+#
+# Copyright © 2018-2021, Commonwealth Scientific and Industrial Research
+# Organisation (CSIRO) ABN 41 687 119 230. Licensed under the CSIRO Open Source
+# Software Licence Agreement.
+#
+
+java $JAVA_OPTS -Djava.library.path=/opt/hadoop/lib/native -ea -jar /pathling.jar
