5076ebf805a42dbb0db0ab0479ca3b731e7a6b8b
John Grimes <John.Grimes@csiro.au>
2021-03-31 21:01:29 +1000

    Fix source location in S3 script

--- site/scripts/publish.sh
+++ site/scripts/publish.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-aws s3 sync ../_site $1 --storage-class REDUCED_REDUNDANCY \
-    --cache-control public,max-age=86400
+aws s3 sync target/site $1 --storage-class REDUCED_REDUNDANCY \
+    --cache-control public,max-age=86400 --dryrun
