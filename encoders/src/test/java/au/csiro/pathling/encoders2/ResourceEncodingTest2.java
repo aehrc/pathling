c1173e021ed2d9f5423513442705bd0040ce655b
Piotr Szul <piotr.szul@csiro.au>
2021-11-23 15:14:36 +1000

    Added test encoding/decoding of all resources

--- encoders/src/test/java/au/csiro/pathling/encoders2/ResourceEncodingTest2.java
+++ encoders/src/test/java/au/csiro/pathling/encoders2/ResourceEncodingTest2.java
@@ -1,66 +1,85 @@
 package au.csiro.pathling.encoders2;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import au.csiro.pathling.encoders.FhirEncoders;
-import au.csiro.pathling.encoders.SchemaConverter;
-import au.csiro.pathling.encoders.datatypes.R4DataTypeMappings;
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.context.RuntimeResourceDefinition;
 import com.google.common.collect.ImmutableSet;
+import java.util.Collection;
 import java.util.Set;
-import org.apache.spark.sql.types.StructType;
+import java.util.stream.Collectors;
+import org.apache.spark.sql.catalyst.InternalRow;
+import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder;
+import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder.Serializer;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.r4.model.Base;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class ResourceEncodingTest2 {
 
-  private final FhirContext fhirContext = FhirContext.forR4();
-  private final R4DataTypeMappings dataTypeMappings = new R4DataTypeMappings();
-
-  private final SchemaConverter converter = new SchemaConverter(fhirContext,
-      dataTypeMappings, 0);
+  private final static FhirContext fhirContext = FhirContext.forR4();
+  private final static FhirEncoders fhirEncoders = FhirEncoders.forR4().getOrCreate();
 
 
-  private final SchemaConverter2 converter2 = new SchemaConverter2(fhirContext, dataTypeMappings,
-      0);
+  // TODO: Remove when the corresponding issues are fixed (#375)
+  private final static Set<String> EXCLUDED_RESOURCES = ImmutableSet.of(
+      "Parameters",
+      // Collections are not supported for custom encoders for: condition-> RuntimeIdDatatypeDefinition[id, IdType]
+      "Task",
+      // Collections are not supported for custom encoders for: condition-> RuntimeIdDatatypeDefinition[id, IdType]
+      "StructureDefinition",
+      // Collections are not supported for custom encoders for: condition-> RuntimeIdDatatypeDefinition[id, IdType]
+      "MolecularSequence",
+      // Collections are not supported for custom encoders for: precision-> RuntimePrimitiveDatatypeDefinition[decimal, DecimalType]
+      "StructureMap",
+      // Collections are not supported for custom encoders for: condition-> RuntimeIdDatatypeDefinition[id, IdType]
+      "Bundle",
+      // scala.MatchError: RuntimeElementDirectResource[DirectChildResource, IBaseResource] (of class ca.uhn.fhir.context.RuntimeElementDirectResource)
+      "PlanDefinition"
+      // Collections are not supported for custom encoders for: goalId-> RuntimeIdDatatypeDefinition[id, IdType]
+  );
 
+  private final Class<? extends IBaseResource> resourceClass;
 
-  private final FhirEncoders fhirEncoders = FhirEncoders.forR4().getOrCreate();
-
-  @Test
-  public void testCanEncodeDecodeAllR4Resources() {
-
-    // TODO: Remove when the corresponding issues are fixed (#375)
-    final Set<String> excludeResources = ImmutableSet.of(
-        "Parameters",
-        // Collections are not supported for custom encoders for: condition-> RuntimeIdDatatypeDefinition[id, IdType]
-        "Task",
-        // Collections are not supported for custom encoders for: condition-> RuntimeIdDatatypeDefinition[id, IdType]
-        "StructureDefinition",
-        // Collections are not supported for custom encoders for: condition-> RuntimeIdDatatypeDefinition[id, IdType]
-        "MolecularSequence",
-        // Collections are not supported for custom encoders for: precision-> RuntimePrimitiveDatatypeDefinition[decimal, DecimalType]
-        "StructureMap",
-        // Collections are not supported for custom encoders for: condition-> RuntimeIdDatatypeDefinition[id, IdType]
-        "Bundle",
-        // scala.MatchError: RuntimeElementDirectResource[DirectChildResource, IBaseResource] (of class ca.uhn.fhir.context.RuntimeElementDirectResource)
-        "PlanDefinition"
-        // Collections are not supported for custom encoders for: goalId-> RuntimeIdDatatypeDefinition[id, IdType]
-    );
-
-    for (String resourceType : fhirContext.getResourceTypes()) {
-      RuntimeResourceDefinition rd = fhirContext
-          .getResourceDefinition(resourceType);
-
-      if (!excludeResources.contains(rd.getName())) {
-        final StructType schema = converter
-            .resourceSchema(rd.getImplementingClass());
-
-        final StructType schema2 = converter2.resourceSchema(rd.getImplementingClass());
-        //schema2.printTreeString();
-        assertEquals("Failed on: " + rd, schema.treeString(), schema2.treeString());
-      }
-    }
+  public ResourceEncodingTest2(Class<? extends IBaseResource> resourceClass) {
+    this.resourceClass = resourceClass;
   }
 
+
+  @Parameters(name = "{index}: class = {0}")
+  public static Collection<?> input() {
+    return fhirContext.getResourceTypes().stream()
+        .filter(rn -> !EXCLUDED_RESOURCES.contains(rn))
+        .map(fhirContext::getResourceDefinition)
+        .map(RuntimeResourceDefinition::getImplementingClass)
+        .map(cls -> new Object[]{cls})
+        .collect(Collectors.toList());
+  }
+
+  @Test
+  public void testCanEncodeDecodeAllR4Resources() throws Exception {
+
+    ExpressionEncoder<? extends IBaseResource> encoder = fhirEncoders
+        .of(resourceClass);
+    ExpressionEncoder<? extends IBaseResource> resolvedEncoder = EncoderUtils
+        .defaultResolveAndBind(encoder);
+
+    IBaseResource resourceInstance = resourceClass.getDeclaredConstructor().newInstance();
+    resourceInstance.setId("someId");
+
+    Serializer<? extends IBaseResource> serializer = resolvedEncoder
+        .createSerializer();
+
+    //noinspection unchecked
+    final InternalRow serializedRow = ((Serializer<IBaseResource>) serializer)
+        .apply(resourceInstance);
+
+    IBaseResource deserializedResource = resolvedEncoder.createDeserializer().apply(serializedRow);
+    assertTrue(((Base) resourceInstance).equalsDeep((Base) deserializedResource));
+  }
 }
