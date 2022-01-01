3508cc0d1608782e7ad1fe29c1d32ae8e73585d8
John Grimes <John.Grimes@csiro.au>
2022-01-01 21:25:31 +1000

    Check ownership of job within result operation

--- fhir-server/src/main/java/au/csiro/pathling/extract/ResultProvider.java
+++ fhir-server/src/main/java/au/csiro/pathling/extract/ResultProvider.java
@@ -6,8 +6,13 @@
 
 package au.csiro.pathling.extract;
 
+import static au.csiro.pathling.security.SecurityAspect.getCurrentUserId;
 import static au.csiro.pathling.utilities.Preconditions.checkNotNull;
 
+import au.csiro.pathling.Configuration;
+import au.csiro.pathling.async.Job;
+import au.csiro.pathling.async.JobRegistry;
+import au.csiro.pathling.errors.AccessDeniedError;
 import au.csiro.pathling.errors.ResourceNotFoundError;
 import au.csiro.pathling.io.ResultReader;
 import au.csiro.pathling.security.OperationAccess;
@@ -15,6 +20,7 @@ import ca.uhn.fhir.rest.annotation.Operation;
 import ca.uhn.fhir.rest.annotation.OperationParam;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Optional;
 import java.util.regex.Pattern;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -22,6 +28,8 @@ import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 /**
@@ -35,17 +43,28 @@ public class ResultProvider {
   private static final Pattern ID_PATTERN = Pattern.compile("^\\w{1,50}$");
 
   @Nonnull
+  private final Configuration configuration;
+
+  @Nonnull
+  private final JobRegistry jobRegistry;
+
+  @Nonnull
   private final ResultRegistry resultRegistry;
 
   @Nonnull
   private final ResultReader resultReader;
 
   /**
+   * @param configuration a {@link Configuration} for determining if authorization is enabled
+   * @param jobRegistry the {@link JobRegistry} used to look up the details of corresponding jobs
    * @param resultRegistry a {@link ResultRegistry} from which to retrieve result URLs
    * @param resultReader a {@link ResultReader} for reading in result files
    */
-  public ResultProvider(@Nonnull final ResultRegistry resultRegistry,
+  public ResultProvider(@Nonnull final Configuration configuration,
+      @Nonnull final JobRegistry jobRegistry, @Nonnull final ResultRegistry resultRegistry,
       @Nonnull final ResultReader resultReader) {
+    this.configuration = configuration;
+    this.jobRegistry = jobRegistry;
     this.resultRegistry = resultRegistry;
     this.resultReader = resultReader;
   }
@@ -73,6 +92,18 @@ public class ResultProvider {
     // Check that the result exists.
     if (resultUrl == null) {
       throw new ResourceNotFoundError("Result ID not found");
+    }
+
+    if (configuration.getAuth().isEnabled()) {
+      // Find the corresponding job.
+      @Nullable final Job job = jobRegistry.get(id);
+      checkNotNull(job);
+      // Check that the user requesting the result is the same user that started the job.
+      final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+      final Optional<String> currentUserId = getCurrentUserId(authentication);
+      if (!job.getOwnerId().equals(currentUserId)) {
+        throw new AccessDeniedError("The requested result is not owned by the current user");
+      }
     }
 
     // Open an input stream to read the result.
