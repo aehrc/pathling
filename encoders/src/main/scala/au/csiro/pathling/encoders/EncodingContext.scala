b9f7fadc837f3b780a01de08acbd984f9333a8a4
John Grimes <John.Grimes@csiro.au>
2022-01-01 12:56:47 +1000

    Update copyright

--- encoders/src/main/scala/au/csiro/pathling/encoders/EncodingContext.scala
+++ encoders/src/main/scala/au/csiro/pathling/encoders/EncodingContext.scala
@@ -5,9 +5,10 @@
  * Bunsen is copyright 2017 Cerner Innovation, Inc., and is licensed under
  * the Apache License, version 2.0 (http://www.apache.org/licenses/LICENSE-2.0).
  *
- * These modifications are copyright © 2018-2021, Commonwealth Scientific
+ * These modifications are copyright © 2018-2022, Commonwealth Scientific
  * and Industrial Research Organisation (CSIRO) ABN 41 687 119 230. Licensed
  * under the CSIRO Open Source Software Licence Agreement.
+ *
  */
 
 package au.csiro.pathling.encoders
--- encoders/src/main/scala/au/csiro/pathling/encoders2/SchemaConverter2.scala
+++ encoders/src/main/scala/au/csiro/pathling/encoders2/SchemaConverter2.scala
@@ -1,3 +1,16 @@
+/*
+ * This is a modified version of the Bunsen library, originally published at
+ * https://github.com/cerner/bunsen.
+ *
+ * Bunsen is copyright 2017 Cerner Innovation, Inc., and is licensed under
+ * the Apache License, version 2.0 (http://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * These modifications are copyright © 2018-2022, Commonwealth Scientific
+ * and Industrial Research Organisation (CSIRO) ABN 41 687 119 230. Licensed
+ * under the CSIRO Open Source Software Licence Agreement.
+ *
+ */
+
 package au.csiro.pathling.encoders2
 
 import au.csiro.pathling.encoders.SchemaConverter
