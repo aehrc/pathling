69dab52d838fad8e4038e82b43c67e71f93526da
John Grimes <John.Grimes@csiro.au>
2022-04-27 17:32:50 +1000

    Move generation of shaded JAR to python POM

--- lib/python/setup.py
+++ lib/python/setup.py
@@ -22,18 +22,18 @@ if not __version__:
 #
 # Check for existence of uber-jar for packaging
 #
-JARS_DIR = os.path.join(HERE, 'target', 'dependency')
-UBER_JAR_GLOB = os.path.join(JARS_DIR, 'fhir-server-*-all.jar')
+JARS_DIR = os.path.join(HERE, 'target')
+UBER_JAR_GLOB = os.path.join(JARS_DIR, 'python-*-all.jar')
 jar_files = glob.glob(UBER_JAR_GLOB)
 if not jar_files:
     print(
-        "ERROR: Cannot find encoders uber-jar in: '%s'.\nMaybe try to run 'mvn package' ..." % JARS_DIR,
-        file=sys.stderr)
+            "ERROR: Cannot find encoders uber-jar in: '%s'.\nMaybe try to run 'mvn package' ..." % JARS_DIR,
+            file=sys.stderr)
     exit(1)
 if len(jar_files) > 1:
     print(
-        "ERROR: Multiple encoders uber-jars found in: '%s'.\nMaybe try to run 'mvn clean' ..." % JARS_DIR,
-        file=sys.stderr)
+            "ERROR: Multiple encoders uber-jars found in: '%s'.\nMaybe try to run 'mvn clean' ..." % JARS_DIR,
+            file=sys.stderr)
     exit(1)
 #
 # Read the long description
@@ -42,46 +42,47 @@ with open('README.md') as f:
     long_description = f.read()
 
 setup(
-    name='pathling',
-    packages=find_packages() + ['pathling.jars'],  # this must be the same as the name above
-    version=__version__,
-    description='Python API to Pathling',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author='Piotr Szul',
-    author_email='piotr.szul@csiro.au',
-    url='https://github.com/aehrc/pathling',
-    keywords=[
-        "pathling",
-        "fhir",
-        "analytics",
-        "spark",
-        "standards",
-        "terminology"
-    ],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: Other/Proprietary License',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-    ],
-    license="CSIRO Open Source Software Licence Agreement (variation of the BSD / MIT License)",
-    python_requires=">=3.7",
-    install_requires=[
-        'pyspark>=3.1.0'
-    ],
-    include_package_data=True,
-    package_dir={
-        'pathling.jars': 'target/dependency',
-    },
-    package_data={
-        'pathling.jars': ['*-all.jar'],
-    },
-    data_files=[
-        ('share/pathling/examples', glob.glob('examples/*.py')),
-        ('share/pathling/examples/data/resources', glob.glob('examples/data/resources/*.ndjson')),
-        ('share/pathling/examples/data/bundles', glob.glob('examples/data/bundles/*.json')),
-    ],
+        name='pathling',
+        packages=find_packages() + ['pathling.jars'],  # this must be the same as the name above
+        version=__version__,
+        description='Python API to Pathling',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        author='Piotr Szul',
+        author_email='piotr.szul@csiro.au',
+        url='https://github.com/aehrc/pathling',
+        keywords=[
+            "pathling",
+            "fhir",
+            "analytics",
+            "spark",
+            "standards",
+            "terminology"
+        ],
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'License :: Other/Proprietary License',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
+        ],
+        license="CSIRO Open Source Software Licence Agreement (variation of the BSD / MIT License)",
+        python_requires=">=3.7",
+        install_requires=[
+            'pyspark>=3.1.0'
+        ],
+        include_package_data=True,
+        package_dir={
+            'pathling.jars': 'target',
+        },
+        package_data={
+            'pathling.jars': ['*-all.jar'],
+        },
+        data_files=[
+            ('share/pathling/examples', glob.glob('examples/*.py')),
+            ('share/pathling/examples/data/resources',
+             glob.glob('examples/data/resources/*.ndjson')),
+            ('share/pathling/examples/data/bundles', glob.glob('examples/data/bundles/*.json')),
+        ],
 )
