1f99f0a31f81c913eecd8f5a2ff5e2948f219ede
John Grimes <John.Grimes@csiro.au>
2022-06-15 10:15:26 +1000

    Update Python package installation notes

--- site/src/components/installation.jsx
+++ site/src/components/installation.jsx
@@ -3,7 +3,8 @@ import React from "react";
 export const PythonInstallation = () => <p>
 To use the Pathling encoders from Python, install
 the <a href="https://pypi.org/project/pathling/">pathling</a> package
-using <a href="https://pip.pypa.io/">pip</a>.
+using <a href="https://pip.pypa.io/">pip</a>. Note that Java 11 or later is required, with your 
+`JAVA_HOME` properly set.
 </p>;
 
 export const ScalaInstallation = () => <p>
