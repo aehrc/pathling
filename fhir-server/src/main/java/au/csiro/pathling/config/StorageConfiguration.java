1a8c9352c07ff5b4f25ae98ae19c6bcbf28158c4
John Grimes <John.Grimes@csiro.au>
2022-10-14 20:34:22 +1000

    Enable arbitrary configuration of Hadoop AWS through config variables

--- fhir-server/src/main/java/au/csiro/pathling/config/StorageConfiguration.java
+++ fhir-server/src/main/java/au/csiro/pathling/config/StorageConfiguration.java
@@ -6,15 +6,10 @@
 
 package au.csiro.pathling.config;
 
-import java.util.Optional;
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import lombok.Data;
-import lombok.ToString;
 
 /**
  * Configuration relating to the storage of data.
@@ -37,56 +32,5 @@ public class StorageConfiguration {
   @Pattern(regexp = "[A-Za-z0-9-_]+")
   @Size(min = 1, max = 50)
   private String databaseName;
-
-  @NotNull
-  private Aws aws;
-
-  /**
-   * Configuration relating to storage of data using Amazon Web Services (AWS).
-   */
-  @Data
-  public static class Aws {
-
-    /**
-     * Public buckets can be accessed by default, set this to false to access protected buckets.
-     */
-    @NotNull
-    private boolean anonymousAccess;
-
-    /**
-     * Authentication details for connecting to a protected Amazon S3 bucket.
-     */
-    @Nullable
-    private String accessKeyId;
-
-    /**
-     * Authentication details for connecting to a protected Amazon S3 bucket.
-     */
-    @Nullable
-    @ToString.Exclude
-    private String secretAccessKey;
-
-    /**
-     * The ARN of an IAM role that should be assumed using STS.
-     */
-    @Nullable
-    private String assumedRole;
-
-    @Nonnull
-    public Optional<String> getAccessKeyId() {
-      return Optional.ofNullable(accessKeyId);
-    }
-
-    @Nonnull
-    public Optional<String> getSecretAccessKey() {
-      return Optional.ofNullable(secretAccessKey);
-    }
-
-    @Nonnull
-    public Optional<String> getAssumedRole() {
-      return Optional.ofNullable(assumedRole);
-    }
-
-  }
 
 }
