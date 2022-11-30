adbfafb5556b0626ef7e9c96d0356a13316c5e76
Piotr Szul <piotr.szul@csiro.au>
2022-11-30 14:39:16 +1000

    Small refactoring and cleanup.

--- terminology/src/main/java/au/csiro/pathling/terminology/TerminologyService2.java
+++ terminology/src/main/java/au/csiro/pathling/terminology/TerminologyService2.java
@@ -51,11 +51,17 @@ public interface TerminologyService2 {
   ConceptSubsumptionOutcome subsumes(@Nonnull Coding codingA, @Nonnull Coding codingB);
 
 
+  /**
+   * Common interface for properties and designations
+   */
   interface PropertyOrDesignation {
     // marker interface
   }
 
 
+  /**
+   * The representation of the property of a concept.
+   */
   @Value(staticConstructor = "of")
   class Property implements PropertyOrDesignation {
 
@@ -64,9 +70,21 @@ public interface TerminologyService2 {
     @Nonnull
     Type value;
 
+    /**
+     * Gets the string representation of the property value.
+     *
+     * @return the string representation of the property value
+     */
     @Nonnull
     public String getValueAsString() {
       return value.primitiveValue();
+    }
+
+    @Override
+    public int hashCode() {
+      // not supported for now because it's not possible to satisfy the hashCode/equals contract
+      // without some form of deepHash corresponding to equalsDeep()
+      throw new UnsupportedOperationException("hashCode not implemented.");
     }
 
     @Override
@@ -86,19 +104,14 @@ public interface TerminologyService2 {
       return value.equalsDeep(property.value);
     }
 
-    @Override
-    public int hashCode() {
-      int result = code.hashCode();
-      result = 31 * result + value.hashCode();
-      return result;
-    }
   }
 
   /**
    * Looks up the properties and designations of given coding in the terminology server.
    *
    * @param coding the coding to lookup.
-   * @param property the code of the property to lookup.
+   * @param property the code of the property to lookup. If not null only the properties with
+   * matching names are returned.
    * @param displayLanguage the language to use for narrowing down designations.
    * @return the list of properties and/or designations.
    */
