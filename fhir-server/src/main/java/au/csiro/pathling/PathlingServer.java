417d68a9ed0e7774bbb812a49e2b955a32714cf1
John Grimes <John.Grimes@csiro.au>
2022-12-30 20:39:12 +1000

    Move banner to file

--- fhir-server/src/main/java/au/csiro/pathling/PathlingServer.java
+++ fhir-server/src/main/java/au/csiro/pathling/PathlingServer.java
@@ -17,17 +17,12 @@
 
 package au.csiro.pathling;
 
-import static au.csiro.pathling.utilities.Preconditions.checkNotNull;
-
 import au.csiro.pathling.config.Configuration;
-import java.io.PrintStream;
-import javax.annotation.Nullable;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
-import org.springframework.core.env.Environment;
 
 /**
  * Main class that serves as the entry point for running the Pathling server.
@@ -44,24 +39,7 @@ public class PathlingServer {
    * @param args Arguments that will be passed to the main application.
    */
   public static void main(final String[] args) {
-    new SpringApplicationBuilder(PathlingServer.class)
-        .banner(new Banner())
-        .run(args);
-  }
-
-  private static class Banner implements org.springframework.boot.Banner {
-
-    @Override
-    public void printBanner(@Nullable final Environment environment,
-        @Nullable final Class<?> sourceClass, @Nullable final PrintStream out) {
-      checkNotNull(out);
-      out.println("    ____        __  __    ___              ");
-      out.println("   / __ \\____ _/ /_/ /_  / (_)___  ____ _ ");
-      out.println("  / /_/ / __ `/ __/ __ \\/ / / __ \\/ __ `/");
-      out.println(" / ____/ /_/ / /_/ / / / / / / / / /_/ /   ");
-      out.println("/_/    \\__,_/\\__/_/ /_/_/_/_/ /_/\\__, / ");
-      out.println("                                /____/");
-    }
+    new SpringApplicationBuilder(PathlingServer.class).run(args);
   }
 
 }
