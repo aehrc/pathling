e626f3831bec8bbd01d94511d18b7c3019b19fd6
Piotr Szul <piotr.szul@csiro.au>
2024-04-17 10:59:02 +1000

    Removing dependency of bulkclient on amazon-sdk-bundle

--- bulkclient/src/test/java/au/csiro/pathling/export/fs/LocalFileStoreTest.java
+++ bulkclient/src/test/java/au/csiro/pathling/export/fs/LocalFileStoreTest.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2023 Commonwealth Scientific and Industrial Research
  * Organisation (CSIRO) ABN 41 687 119 230.
- *  
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *  
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,13 +22,13 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import au.csiro.pathling.export.fs.FileStore.FileHandle;
-import com.amazonaws.util.StringInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
@@ -80,16 +80,15 @@ public class LocalFileStoreTest {
   @Test
   void testWriteAllWorks() throws IOException {
     final File file = testRootDir.resolve("file").toFile();
-    fileStore.get(file.getPath()).writeAll(new StringInputStream("Hello, world!"));
+    fileStore.get(file.getPath())
+        .writeAll(IOUtils.toInputStream("Hello, world!", StandardCharsets.UTF_8));
     assertTrue(file.exists());
     assertEquals("Hello, world!", FileUtils.readFileToString(file, StandardCharsets.UTF_8));
   }
-  
+
   @Test
   void testToUriWorks() {
     assertEquals("/foo/bar", FileHandle.ofLocal("/foo/bar").getLocation());
     assertEquals(URI.create("file:/foo/bar"), FileHandle.ofLocal("/foo/bar").toUri());
   }
-
-
 }
