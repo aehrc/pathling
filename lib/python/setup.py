24e4d8f790dac53093bd4118a2fd5a24c62c73d7
Piotr Szul <piotr.szul@csiro.au>
2024-04-18 11:57:54 +1000

    Updating pyspark version to 3.5.0 and minimal python version to 3.8 (as per spark 3.5 requirements).

--- lib/python/setup.py
+++ lib/python/setup.py
@@ -56,7 +56,6 @@ setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
