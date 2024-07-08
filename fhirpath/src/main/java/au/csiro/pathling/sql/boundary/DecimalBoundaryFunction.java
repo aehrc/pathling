c16a7e052d961775236de387e64d59e9f96df94e
John Grimes <John.Grimes@csiro.au>
2024-07-08 14:10:12 +1000

    Update boundary function tests and get passing

--- fhirpath/src/main/java/au/csiro/pathling/sql/boundary/DecimalBoundaryFunction.java
+++ fhirpath/src/main/java/au/csiro/pathling/sql/boundary/DecimalBoundaryFunction.java
@@ -14,6 +14,10 @@ public abstract class DecimalBoundaryFunction {
   @Nullable
   protected static BigDecimal lowBoundaryForDecimal(@Nullable final BigDecimal d,
       @Nullable final Integer precision) throws Exception {
+    final Optional<Integer> maxScale = getMaxScale(d, precision);
+    if (maxScale.isEmpty()) {
+      return null;
+    }
     return new LowBoundaryForDecimal().call(d,
         Optional.ofNullable(precision).orElse(MAX_PRECISION));
   }
@@ -21,8 +25,27 @@ public abstract class DecimalBoundaryFunction {
   @Nullable
   protected static BigDecimal highBoundaryForDecimal(@Nullable final BigDecimal d,
       @Nullable final Integer precision) throws Exception {
+    final Optional<Integer> maxScale = getMaxScale(d, precision);
+    if (maxScale.isEmpty()) {
+      return null;
+    }
     return new HighBoundaryForDecimal().call(d,
         Optional.ofNullable(precision).orElse(MAX_PRECISION));
+  }
+
+  private static Optional<Integer> getMaxScale(@Nullable final BigDecimal d,
+      @Nullable final Integer precision) {
+    if (d == null) {
+      return Optional.empty();
+    }
+    final int integerLength = d.precision() - d.scale();
+    final int maxScale = MAX_PRECISION - integerLength;
+    if (precision == null) {
+      return Optional.of(maxScale);
+    }
+    return precision >= 0 && precision <= maxScale
+           ? Optional.of(maxScale)
+           : Optional.empty();
   }
 
 }
