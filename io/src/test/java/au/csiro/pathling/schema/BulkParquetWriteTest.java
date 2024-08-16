3b3c0dccf78f4f61954ab4d33cfa4d7ff2876712
John Grimes <John.Grimes@csiro.au>
2024-08-16 17:46:22 +1000

    Copy test data into io and use within BulkParquetWriteTest

---
+++ io/src/test/java/au/csiro/pathling/schema/BulkParquetWriteTest.java
@@ -0,0 +1,43 @@
+package au.csiro.pathling.schema;
+
+import au.csiro.pathling.test.TestResources;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
+import org.apache.spark.sql.SparkSession;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BulkParquetWriteTest {
+
+  Path tempDirectory;
+
+  @BeforeEach
+  void setUp() throws IOException {
+    tempDirectory = Files.createTempDirectory("pathling-BulkParquetWriteTest");
+  }
+
+  @Test
+  void test() {
+    SparkSession.builder()
+        .master("local[*]")
+        .getOrCreate();
+    final FhirJsonReader reader = new FhirJsonReader("ExplanationOfBenefit", "R4",
+        Map.of("multiLine", "false")
+    );
+
+    final URL testData = TestResources.getResourceAsUrl(
+        "bulk/fhir/ExplanationOfBenefit.ndjson");
+    final Dataset<Row> data = reader.read(testData.toString());
+    data.write()
+        .format("parquet")
+        .mode(SaveMode.Overwrite)
+        .save(tempDirectory.resolve("parquet/ExplanationOfBenefit.parquet").toString());
+  }
+
+}
