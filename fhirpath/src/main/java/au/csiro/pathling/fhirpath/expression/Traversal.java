85c75ce28e0c783a598b65672edeb1c84a48cfb5
John Grimes <John.Grimes@csiro.au>
2024-11-07 12:03:47 +1000

    Handle optional specification of type at root

--- fhirpath/src/main/java/au/csiro/pathling/fhirpath/expression/Traversal.java
+++ fhirpath/src/main/java/au/csiro/pathling/fhirpath/expression/Traversal.java
@@ -10,6 +10,13 @@ public record Traversal(Optional<FhirPath> source, String target) implements Fhi
 
   @Override
   public @NotNull Collection evaluate(final @NotNull Collection input, final @NotNull EvaluationContext context) {
+    // If we are at the root context and the target is the same as the input type, this is just a
+    // reference to the input type itself and is a no-op.
+    if (source.isEmpty() && 
+        input.getType().isPresent() && 
+        input.getType().get().getTypeName().equals(target)) {
+      return input;
+    }
     return input.traverse(target);
   }
 
