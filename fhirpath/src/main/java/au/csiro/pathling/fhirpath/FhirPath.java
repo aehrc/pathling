7a088dd32f6a86277b0cd4199a2b39569dba6d89
John Grimes <John.Grimes@csiro.au>
2024-11-08 16:49:23 +1000

    Remove unnecessary method FhirPath.traverse

--- fhirpath/src/main/java/au/csiro/pathling/fhirpath/FhirPath.java
+++ fhirpath/src/main/java/au/csiro/pathling/fhirpath/FhirPath.java
@@ -2,9 +2,7 @@ package au.csiro.pathling.fhirpath;
 
 import au.csiro.pathling.fhirpath.collection.Collection;
 import au.csiro.pathling.fhirpath.evaluation.EvaluationContext;
-import au.csiro.pathling.fhirpath.expression.Traversal;
 import com.google.gson.Gson;
-import java.util.Optional;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -17,10 +15,6 @@ public interface FhirPath {
 
   @NotNull Collection evaluate(@NotNull final Collection input,
       @NotNull final EvaluationContext context);
-
-  default @NotNull FhirPath traverse(@NotNull final String target) {
-    return new Traversal(Optional.of(this), target);
-  }
 
   default @NotNull String toJson(final @NotNull Gson gson) {
     return "{\"" + this.getClass().getSimpleName() + "\": "
