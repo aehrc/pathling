207c3a462cd8af443f54fa584b446f7372198a05
Piotr Szul <piotr.szul@csiro.au>
2025-01-24 10:11:47 +1000

    Adding support for join expressions (resolve,reverseResolve) in aggregations for AggregateQueryExecutor.

--- fhirpath/src/main/java/au/csiro/pathling/aggregate/AggregateQueryExecutor.java
+++ fhirpath/src/main/java/au/csiro/pathling/aggregate/AggregateQueryExecutor.java
@@ -23,11 +23,9 @@ import au.csiro.pathling.QueryExecutor;
 import au.csiro.pathling.config.QueryConfiguration;
 import au.csiro.pathling.fhirpath.FhirPath;
 import au.csiro.pathling.fhirpath.execution.FhirpathEvaluator;
+import au.csiro.pathling.fhirpath.execution.JoinResolver;
 import au.csiro.pathling.fhirpath.execution.MultiFhirpathEvaluator.ManyFactory;
-import au.csiro.pathling.fhirpath.execution.SingleFhirpathEvaluator;
-import au.csiro.pathling.fhirpath.function.registry.StaticFunctionRegistry;
 import au.csiro.pathling.fhirpath.parser.Parser;
-import au.csiro.pathling.fhirpath.path.Paths;
 import au.csiro.pathling.io.Database;
 import au.csiro.pathling.io.source.DataSource;
 import au.csiro.pathling.sql.SqlExpressions;
@@ -35,7 +33,6 @@ import au.csiro.pathling.terminology.TerminologyServiceFactory;
 import ca.uhn.fhir.context.FhirContext;
 import jakarta.annotation.Nonnull;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Stream;
 import lombok.Value;
@@ -122,20 +119,17 @@ public class AggregateQueryExecutor extends QueryExecutor {
 
     final List<EvaluatedPath> evaluatedGoupings = evalPaths(grouppingPaths, fhirEvaluator);
 
-    // get the current resource
-    final List<EvaluatedPath> evaluatedValues = evalPaths(List.of(new Paths.This()),
-        fhirEvaluator);
-
+    // prepend the materialized grouping columns to the dataset
     final Dataset<Row> inputDataset = filteredDataset.select(
-        Stream.of(evaluatedValues, evaluatedGoupings)
-            .flatMap(List::stream)
-            .map(c -> c.getColumnValue().alias(randomAlias()))
-            .toArray(Column[]::new)
+        Stream.concat(
+            evaluatedGoupings.stream().map(c -> c.getColumnValue().alias(randomAlias())),
+            Stream.of(filteredDataset.columns()).map(functions::col)
+        ).toArray(Column[]::new)
     );
 
-    // now we need to explode all array columns in the dataset
+    // explode the grouping columns
     final Dataset<Row> expandeDataset = Stream.of(inputDataset.schema().fields())
-        .skip(1)
+        .limit(evaluatedGoupings.size())
         .reduce(inputDataset, (dataset, field) -> {
           if (field.dataType() instanceof ArrayType) {
             return dataset.withColumn(field.name(),
@@ -144,36 +138,31 @@ public class AggregateQueryExecutor extends QueryExecutor {
           return dataset;
         }, (dataset1, dataset2) -> dataset1);
 
-    // then groupBy the groupings and aggregate with the relevant combiner function
-    // The combiner function is used to aggregate partial aggregation results from agg fhirpaths.
-    // For example for `count()` the aggregation is SQL SUM(), for 
-
-    final Column valueColumn = Stream.of(expandeDataset.columns())
-        .limit(1)
-        .map(expandeDataset::col)
-        .findFirst().orElseThrow();
-
-    final Column[] groupingColumns = Stream.of(expandeDataset.columns())
-        .skip(1)
+    // normalize the grouping columns (prune the synthetic fields)
+    final Column[] normalizedGroupingColumns = Stream.of(expandeDataset.columns())
+        .limit(evaluatedGoupings.size())
         .map(c -> SqlExpressions.pruneSyntheticFields(functions.col(c)).alias(c))
         .toArray(Column[]::new);
 
-    final FhirpathEvaluator aggEvaluator = SingleFhirpathEvaluator.of(query.getSubjectResource(),
-        fhirContext,
-        StaticFunctionRegistry.getInstance(),
-        Map.of(), dataSource);
+    // compute the aggregation from the initial dataset
+    final List<Column> dataColumns = Stream.of(filteredDataset.columns())
+        // for external referenced use collect map otherwise use collect list
+        .map(c -> c.contains("@")
+                  ? JoinResolver.collect_map(functions.col(c)).alias(c)
+                  : functions.collect_list(functions.col(c)).alias(c))
+        .toList();
 
     //then we need to group by the grouping columns and collect the resource column to a list
-    final Dataset<Row> grouppedAggSource = expandeDataset.groupBy(groupingColumns)
-        .agg(functions.collect_list(valueColumn).alias(query.getSubjectResource().toCode()));
+    final Dataset<Row> grouppedAggSource = expandeDataset.groupBy(normalizedGroupingColumns)
+        .agg(dataColumns.get(0), dataColumns.subList(1, dataColumns.size()).toArray(Column[]::new));
 
-    final List<EvaluatedPath> evaluatedAggs = evalPaths(aggPaths, aggEvaluator);
+    final List<EvaluatedPath> evaluatedAggs = evalPaths(aggPaths, fhirEvaluator);
     final Column[] aggColumns = evaluatedAggs.stream()
         .map(c -> c.getColumnValue().alias(randomAlias()))
         .toArray(Column[]::new);
 
     final Dataset<Row> resultDataset = grouppedAggSource
-        .select(Stream.concat(Stream.of(groupingColumns), Stream.of(aggColumns))
+        .select(Stream.concat(Stream.of(normalizedGroupingColumns), Stream.of(aggColumns))
             .toArray(Column[]::new));
 
     return new ResultWithExpressions(resultDataset,
