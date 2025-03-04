5bbbe78422fcc179fbe76f916f253b19493a345e
John Grimes <John.Grimes@csiro.au>
2025-03-04 20:22:16 +1000

    Revert "Update license fields in setup.py"
    This reverts commit 1520dfb54186225de5d2f53f4d68e355fd3ecca6.

--- lib/python/setup.py
+++ lib/python/setup.py
@@ -16,9 +16,8 @@
 import glob
 import os
 import sys
-from typing import Union
-
 from setuptools import setup
+from typing import Union
 
 HERE = os.path.dirname(__file__)
 ROOT_DIR = os.path.abspath(HERE)
@@ -61,8 +60,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
     ],
-    license="Apache-2.0",
-    license_files=["LICENSE"],
+    license="Apache License, version 2.0",
     python_requires=">=3.8",
     install_requires=["pyspark>=3.5.3,<3.6.0", "deprecated>=1.2.13"],
     include_package_data=True,
