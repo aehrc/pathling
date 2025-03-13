4da8a15d0048156ae46c4a722f19e5106223495b
Piotr Szul <piotr.szul@csiro.au>
2025-03-13 14:35:26 +1000

    Improving documentation.

--- fhirpath/src/main/java/au/csiro/pathling/fhirpath/execution/DataRoot.java
+++ fhirpath/src/main/java/au/csiro/pathling/fhirpath/execution/DataRoot.java
@@ -21,21 +21,49 @@ import jakarta.annotation.Nonnull;
 import lombok.Value;
 import org.hl7.fhir.r4.model.Enumerations.ResourceType;
 
+/**
+ * Represents a root of data in the FHIR path execution context.
+ * This interface defines the common behavior for different types of data roots
+ * such as resource roots and join roots.
+ */
 public interface DataRoot {
 
-
+  /**
+   * Gets the resource type of this data root.
+   *
+   * @return The resource type
+   */
   @Nonnull
   ResourceType getResourceType();
 
+  /**
+   * Gets the parent resource type of this data root.
+   * For a resource root, this is the same as the resource type.
+   * For a join root, this is the resource type of the master.
+   *
+   * @return The parent resource type
+   */
   @Nonnull
   ResourceType getParentResourceType();
 
+  /**
+   * Returns a human-readable string representation of this data root.
+   *
+   * @return A display string
+   */
   @Nonnull
   String toDisplayString();
 
+  /**
+   * Represents a root of a FHIR resource.
+   * This is the simplest form of data root, representing a direct resource.
+   */
   @Value(staticConstructor = "of")
   class ResourceRoot implements DataRoot {
 
+    /**
+     * The type of the resource.
+     */
     @Nonnull
     ResourceType resourceType;
 
@@ -58,21 +86,46 @@ public interface DataRoot {
     }
   }
 
-
+  /**
+   * Represents a root that is joined to another root.
+   * This interface defines the common behavior for different types of join roots.
+   */
   interface JoinRoot extends DataRoot {
 
+    /**
+     * Gets the parent resource type of this join root.
+     * This is the resource type of the master.
+     *
+     * @return The parent resource type
+     */
     @Override
     @Nonnull
     default ResourceType getParentResourceType() {
       return getMaster().getResourceType();
     }
 
+    /**
+     * Gets the master data root that this join root is joined to.
+     *
+     * @return The master data root
+     */
     @Nonnull
     DataRoot getMaster();
 
+    /**
+     * Converts this join root to a join tag.
+     *
+     * @return The join tag
+     */
     @Nonnull
     JoinTag asTag();
 
+    /**
+     * Gets the tag for this join root.
+     * This is the tag of the join tag.
+     *
+     * @return The tag
+     */
     @Override
     @Nonnull
     default String getTag() {
@@ -80,13 +133,28 @@ public interface DataRoot {
     }
   }
 
+  /**
+   * Represents a root that is joined to another root via a reverse resolve.
+   * This is used when resolving from a resource to resources that reference it.
+   */
   @Value(staticConstructor = "of")
   class ReverseResolveRoot implements JoinRoot {
 
+    /**
+     * The master data root that this reverse resolve root is joined to.
+     */
     @Nonnull
     DataRoot master;
+    
+    /**
+     * The type of the foreign resource that references the master resource.
+     */
     @Nonnull
     ResourceType foreignResourceType;
+    
+    /**
+     * The path in the foreign resource that references the master resource.
+     */
     @Nonnull
     String foreignKeyPath;
 
@@ -108,7 +176,14 @@ public interface DataRoot {
       return foreignResourceType.toCode() + "<-" + foreignKeyPath;
     }
 
-
+    /**
+     * Creates a new reverse resolve root from a resource type.
+     *
+     * @param masterType The type of the master resource
+     * @param foreignResourceType The type of the foreign resource
+     * @param foreignResourcePath The path in the foreign resource that references the master resource
+     * @return A new reverse resolve root
+     */
     public static ReverseResolveRoot ofResource(@Nonnull final ResourceType masterType,
         @Nonnull final ResourceType foreignResourceType,
         @Nonnull final String foreignResourcePath) {
@@ -118,13 +193,28 @@ public interface DataRoot {
 
   }
 
+  /**
+   * Represents a root that is joined to another root via a resolve.
+   * This is used when resolving from a resource to resources that it references.
+   */
   @Value(staticConstructor = "of")
   class ResolveRoot implements JoinRoot {
 
+    /**
+     * The master data root that this resolve root is joined to.
+     */
     @Nonnull
     DataRoot master;
+    
+    /**
+     * The type of the foreign resource that is referenced by the master resource.
+     */
     @Nonnull
     ResourceType foreignResourceType;
+    
+    /**
+     * The path in the master resource that references the foreign resource.
+     */
     @Nonnull
     String masterResourcePath;
 
@@ -146,7 +236,14 @@ public interface DataRoot {
       return masterResourcePath + "->" + foreignResourceType.toCode();
     }
 
-
+    /**
+     * Creates a new resolve root from a resource type.
+     *
+     * @param masterType The type of the master resource
+     * @param foreignResourceType The type of the foreign resource
+     * @param masterResourcePath The path in the master resource that references the foreign resource
+     * @return A new resolve root
+     */
     public static ResolveRoot ofResource(@Nonnull final ResourceType masterType,
         @Nonnull final ResourceType foreignResourceType,
         @Nonnull final String masterResourcePath) {
@@ -155,19 +252,43 @@ public interface DataRoot {
     }
   }
 
+  /**
+   * Gets the tag for this data root.
+   * This is used to identify the data root in the execution context.
+   *
+   * @return The tag
+   */
   @Nonnull
   String getTag();
 
+  /**
+   * Gets the parent key tag for this data root.
+   * This is used to identify the parent key in the execution context.
+   *
+   * @return The parent key tag
+   */
   @Nonnull
   default String getParentKeyTag() {
     return getTag() + "__pkey";
   }
 
+  /**
+   * Gets the child key tag for this data root.
+   * This is used to identify the child key in the execution context.
+   *
+   * @return The child key tag
+   */
   @Nonnull
   default String getChildKeyTag() {
     return getTag() + "__ckey";
   }
 
+  /**
+   * Gets the value tag for this data root.
+   * This is used to identify the value in the execution context.
+   *
+   * @return The value tag
+   */
   @Nonnull
   default String getValueTag() {
     return getTag();
