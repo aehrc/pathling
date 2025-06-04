4a87a6dfd2a9e6ce31ad094d45612f78ca2eb8e5
Piotr Szul <piotr.szul@csiro.au>
2025-06-04 15:39:45 +1000

    refactoring: refactoring FunctionParameterResolver for better testability.

--- fhirpath/src/main/java/au/csiro/pathling/fhirpath/function/FunctionParameterResolver.java
+++ fhirpath/src/main/java/au/csiro/pathling/fhirpath/function/FunctionParameterResolver.java
@@ -1,5 +1,6 @@
 package au.csiro.pathling.fhirpath.function;
 
+import static au.csiro.pathling.utilities.Preconditions.checkUserInput;
 import static java.util.Objects.isNull;
 
 import au.csiro.pathling.fhirpath.Concepts;
@@ -10,17 +11,74 @@ import au.csiro.pathling.fhirpath.collection.Collection;
 import au.csiro.pathling.fhirpath.path.ParserPaths;
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 import lombok.Value;
 
 @Value
-class FunctionParameterResolver {
+public class FunctionParameterResolver {
 
   EvaluationContext evaluationContext;
   Collection input;
+  List<FhirPath> actualArguments;
+
+
+  @Value(staticConstructor = "of")
+  public static class FunctionInvocation {
+
+    Method method;
+    Object[] arguments;
+
+    public Collection invoke() {
+      try {
+        return (Collection) method.invoke(null, arguments);
+      } catch (final IllegalAccessException | InvocationTargetException e) {
+        throw new RuntimeException("Error invoking function: " + method.getName(), e);
+      }
+    }
+
+  }
+
+  @Nonnull
+  public static FunctionParameterResolver fromFunctionInput(
+      @Nonnull final FunctionInput functionInput) {
+    return new FunctionParameterResolver(
+        functionInput.getContext(),
+        functionInput.getInput(),
+        functionInput.getArguments());
+  }
+
+
+  @Nonnull
+  public FunctionInvocation bind(@Nonnull final Method method) {
+    // Check that not extra arguments are provided.
+    checkUserInput(actualArguments.size() <= method.getParameterCount() - 1,
+        "Too many arguments provided for function '" + method.getName() + "'. Expected "
+            + (method.getParameterCount() - 1) + ", got " + actualArguments.size());
+
+    // Resolve each pair of method parameter and argument.
+    final Stream<Object> resolvedArguments = IntStream.range(0, method.getParameterCount() - 1)
+        .mapToObj(i -> resolveArgument(method.getParameters()[i + 1],
+            (i < actualArguments.size())
+            ? actualArguments.get(i)
+            : null));
+
+    // Resolve the input.
+    final Object resolvedInput = resolveCollection(input, method.getParameters()[0]);
+    return FunctionInvocation.of(method,
+        Stream.concat(Stream.of(resolvedInput), resolvedArguments)
+            .toArray(Object[]::new));
+  }
+
 
   @Nullable
-  public Object resolveArgument(@Nonnull final Parameter parameter,
+  Object resolveArgument(@Nonnull final Parameter parameter,
       final FhirPath argument) {
 
     if (isNull(argument)) {
@@ -60,7 +118,7 @@ class FunctionParameterResolver {
     }
   }
 
-  public Object resolveCollection(@Nonnull final Collection collection,
+  Object resolveCollection(@Nonnull final Collection collection,
       @Nonnull final Parameter parameter) {
     if (Concepts.class.isAssignableFrom(parameter.getType())) {
       // evaluate collection types 
