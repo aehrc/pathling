7d1f240b9035d5ac7fb5375d7e1c179094fd86e5
John Grimes <John.Grimes@csiro.au>
2025-06-26 17:17:01 +1000

    Use sample time mode

--- benchmark/src/main/java/au/csiro/pathling/benchmark/PathlingBenchmark.java
+++ benchmark/src/main/java/au/csiro/pathling/benchmark/PathlingBenchmark.java
@@ -2,15 +2,21 @@ package au.csiro.pathling.benchmark;
 
 import jakarta.annotation.Nonnull;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import org.apache.spark.sql.Row;
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Warmup;
 
 @Fork(2)
 @Warmup(iterations = 1, time = 5)
 @Measurement(iterations = 3, time = 5)
+@BenchmarkMode({Mode.SampleTime})
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class PathlingBenchmark {
 
   @Benchmark
