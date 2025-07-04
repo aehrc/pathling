2b851faf22ec34bf5ab7ffce894c382e3e91b21d
John Grimes <John.Grimes@csiro.au>
2025-07-04 16:13:33 +1000

    Re-enable YamlFhirpathTest.testSearchParams

--- fhirpath/src/test/java/au/csiro/pathling/fhirpath/yaml/YamlFhirpathTest.java
+++ fhirpath/src/test/java/au/csiro/pathling/fhirpath/yaml/YamlFhirpathTest.java
@@ -6,7 +6,6 @@ import au.csiro.pathling.test.yaml.annotations.YamlTestConfiguration;
 import au.csiro.pathling.test.yaml.executor.YamlTestExecutor;
 import jakarta.annotation.Nonnull;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 
 @Slf4j
@@ -21,7 +20,6 @@ public class YamlFhirpathTest extends YamlCachedTestBase {
     run(testCase);
   }
 
-  @Disabled("Disabled until we add exclusion rules")
   @YamlTest("fhirpath-ptl/cases/search-params.yaml")
   void testSearchParams(@Nonnull final YamlTestExecutor testCase) {
     run(testCase);
