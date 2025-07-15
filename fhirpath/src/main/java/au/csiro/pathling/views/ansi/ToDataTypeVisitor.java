957a9212970c0d64368bbf7f978f715aa5cc1058
Piotr Szul <piotr.szul@csiro.au>
2025-07-15 10:33:06 +1000

    refactoring: adding assertions in default branches of ToDataTypeVisitor

--- fhirpath/src/main/java/au/csiro/pathling/views/ansi/ToDataTypeVisitor.java
+++ fhirpath/src/main/java/au/csiro/pathling/views/ansi/ToDataTypeVisitor.java
@@ -1,5 +1,6 @@
 package au.csiro.pathling.views.ansi;
 
+import static au.csiro.pathling.utilities.Preconditions.check;
 import static java.util.Objects.nonNull;
 import static java.util.Objects.requireNonNull;
 
@@ -24,26 +25,31 @@ public class ToDataTypeVisitor extends TypesOfAnsiSqlBaseVisitor<DataType> {
     this.factory = new AnsiSqlDataTypeFactory();
   }
 
-  private static DataType fail(@Nonnull final String msg) {
-    throw new IllegalStateException(msg);
-  }
-
+  /**
+   * Checks if any of the provided nodes are non-null.
+   *
+   * @param nodes the nodes to check
+   * @return true if any node is non-null, false otherwise
+   */
   private static boolean anyOf(@Nonnull TerminalNode... nodes) {
     return Stream.of(nodes).anyMatch(Objects::nonNull);
   }
 
   @Override
+  @Nonnull
   public DataType visitSqlType(@Nonnull final TypesOfAnsiSqlParser.SqlTypeContext ctx) {
     return visitChildren(ctx);
   }
 
   @Override
+  @Nonnull
   public DataType visitCharacterType(@Nonnull final TypesOfAnsiSqlParser.CharacterTypeContext ctx) {
     if (anyOf(ctx.K_VARCHAR(), ctx.K_VARYING())) {
       return nonNull(ctx.length)
              ? factory.createVarchar(Integer.parseInt(ctx.length.getText()))
              : factory.createVarchar();
     } else {
+      check(anyOf(ctx.K_CHARACTER(), ctx.K_CHAR()) && !anyOf(ctx.K_VARYING()));
       return nonNull(ctx.length)
              ? factory.createCharacter(Integer.parseInt(ctx.length.getText()))
              : factory.createCharacter();
@@ -51,6 +57,7 @@ public class ToDataTypeVisitor extends TypesOfAnsiSqlBaseVisitor<DataType> {
   }
 
   @Override
+  @Nonnull
   public DataType visitNumericType(@Nonnull final TypesOfAnsiSqlParser.NumericTypeContext ctx) {
     if (anyOf(ctx.K_SMALLINT())) {
       return factory.createSmallInt();
@@ -66,7 +73,8 @@ public class ToDataTypeVisitor extends TypesOfAnsiSqlBaseVisitor<DataType> {
       return nonNull(ctx.precision)
              ? factory.createFloat(Integer.parseInt(ctx.precision.getText()))
              : factory.createFloat();
-    } else if (anyOf(ctx.K_NUMERIC(), ctx.K_DECIMAL(), ctx.K_DEC())) {
+    } else {
+      check(anyOf(ctx.K_NUMERIC(), ctx.K_DECIMAL(), ctx.K_DEC()));
       if (ctx.precision != null) {
         int precision = Integer.parseInt(ctx.precision.getText());
         if (ctx.scale != null) {
@@ -76,24 +84,24 @@ public class ToDataTypeVisitor extends TypesOfAnsiSqlBaseVisitor<DataType> {
         return factory.createDecimal(precision);
       }
       return factory.createDecimal();
-    } else {
-      // This should never happen unless there's a bug in the parser or visitor
-      return fail("Unrecognized numeric type: " + ctx.getText());
     }
   }
 
   @Override
+  @Nonnull
   public DataType visitBooleanType(@Nonnull final TypesOfAnsiSqlParser.BooleanTypeContext ctx) {
     return factory.createBoolean();
   }
 
   @Override
+  @Nonnull
   public DataType visitBinaryType(@Nonnull final TypesOfAnsiSqlParser.BinaryTypeContext ctx) {
-    if (anyOf(ctx.K_BINARY(), ctx.K_VARYING())) {
+    if (anyOf(ctx.K_VARBINARY(), ctx.K_VARYING())) {
       return nonNull(ctx.length)
              ? factory.createVarbinary(Integer.parseInt(ctx.length.getText()))
              : factory.createVarbinary();
     } else {
+      check(anyOf(ctx.K_BINARY()) && !anyOf(ctx.K_VARYING()));
       return nonNull(ctx.length)
              ? factory.createBinary(Integer.parseInt(ctx.length.getText()))
              : factory.createBinary();
@@ -101,12 +109,14 @@ public class ToDataTypeVisitor extends TypesOfAnsiSqlBaseVisitor<DataType> {
   }
 
   @Override
+  @Nonnull
   public DataType visitTemporalType(@Nonnull final TypesOfAnsiSqlParser.TemporalTypeContext ctx) {
     if (anyOf(ctx.K_DATE())) {
       return factory.createDate();
     } else if (anyOf(ctx.K_INTERVAL())) {
       return factory.createInterval();
-    } else if (anyOf(ctx.K_TIMESTAMP())) {
+    } else {
+      check(anyOf(ctx.K_TIMESTAMP()));
       boolean withTimeZone = nonNull(ctx.timeZone()) && anyOf(ctx.timeZone().K_WITH());
       if (ctx.precision != null) {
         int precision = Integer.parseInt(ctx.precision.getText());
@@ -117,18 +127,17 @@ public class ToDataTypeVisitor extends TypesOfAnsiSqlBaseVisitor<DataType> {
       return withTimeZone
              ? factory.createTimestampWithTimeZone()
              : factory.createTimestamp();
-    } else {
-      // This should never happen unless there's a bug in the parser or visitor
-      return fail("Unrecognized temporal type: " + ctx.getText());
     }
   }
 
   @Override
+  @Nonnull
   public DataType visitComplexType(@Nonnull final TypesOfAnsiSqlParser.ComplexTypeContext ctx) {
     return visitChildren(ctx);
   }
 
   @Override
+  @Nonnull
   public DataType visitRowType(@Nonnull final TypesOfAnsiSqlParser.RowTypeContext ctx) {
     return factory.createRow(
         requireNonNull(ctx.fieldDefinition()).stream()
@@ -140,6 +149,7 @@ public class ToDataTypeVisitor extends TypesOfAnsiSqlBaseVisitor<DataType> {
   }
 
   @Override
+  @Nonnull
   public DataType visitArrayType(@Nonnull final TypesOfAnsiSqlParser.ArrayTypeContext ctx) {
     final DataType elementType = visit(requireNonNull(ctx.sqlType()));
     return factory.createArray(requireNonNull(elementType));
