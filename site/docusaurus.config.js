6e2c6c1e6f8b72a219f50a2b89164c4813b520ed
Piotr Szul <piotr.szul@csiro.au>
2025-10-23 21:04:43 +1000

    release: Bumping version to 6.1.1-SNAPSHOT

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -52,7 +52,7 @@ const config = {
           lastVersion: "current",
           versions: {
             current: {
-              label: "8.1.0",
+              label: "8.1.1-SNAPSHOT",
               path: "/",
             },
             "7.2.0": {
